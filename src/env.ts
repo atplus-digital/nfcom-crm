d682fb31116dbfaabc6f23f5c5a94e1a081afff7
unknown <gustavo.carbonera@atplus.com.br>
2026-03-03 17:05:39 -0300

    initial commit

---
+++ src/env.ts
@@ -0,0 +1,14 @@
+import { createEnv } from "@t3-oss/env-core";
+import { config } from "dotenv";
+import { z } from "zod";
+
+config();
+
+export const env = createEnv({
+	server: {
+		SERVER_URL: z.string().optional(),
+		DATABASE_URL: z.string(),
+	},
+	runtimeEnv: process.env,
+	emptyStringAsUndefined: true,
+});
